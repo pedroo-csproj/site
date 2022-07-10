bb25c8b627c5ce8c7622c4932c33c891f9c5774a
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-07-10 13:05:01 -0300

    footer removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
 
 import { NotFound } from './pages/not-found';
 import { Home } from './pages/Home';
-
-const Footer = styled.footer`
-  position: fixed;
-  left: 50%;
-  bottom: 10px;
-  transform: translate(-50%, -50%);
-  margin: 0 auto;
-  font-size: 14px;
-`;
 
 function App() {
   return (
@@ -20,9 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <Footer>
-        <p>São Paulo, SP, Brasil</p>
-      </Footer>
     </>
   );
 }
