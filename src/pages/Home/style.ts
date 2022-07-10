6d5582c28f15f915262e77ab10e75c68e260145a
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-07-10 12:32:09 -0300

    hover added to elements

--- src/pages/Home/style.ts
+++ src/pages/Home/style.ts
@@ -1,16 +1,30 @@
 import styled from "styled-components";
 
 export const AboutMe = styled.div`
-    h1 {
-        line-height: 5px;
-    }
-
     img {
-        width: 150px;
-        height: 150px;
         border-radius: 50%;
     }
-`
+
+    @media (min-width: 600px) {
+        img{
+            width: 150px;
+            height: 150px;
+            transition: all 0.3s ease-in-out;
+        }
+
+        h1{
+            line-height: 5px;
+        }
+    }
+
+    @media (max-width: 600px) {
+        img {
+            width: 180px;
+            height: 180px;
+            transition: all 0.3s ease-in-out;
+        }
+    }
+`;
 
 export const Description = styled.div`
     margin-top: 40px;
@@ -18,8 +32,20 @@ export const Description = styled.div`
     p {
         margin: 0 auto;
         text-align: justify;
-        max-width: 60%;
-        font-size: 16px;
+    }
+
+    @media (max-width: 600px) {
+        p{
+            font-size: 18px;
+        }
+    }
+
+    @media (min-width: 600px) {
+        p {
+            text-align: justify;
+            max-width: 70%;
+            font-size: 16px;
+        }
     }
 `;
 
@@ -38,13 +64,25 @@ margin-top: 60px !important;
         color:#f5f5f5;
         text-decoration: none;
         background-color: #333333;
-        padding: 10px 10%;
-        font-size: 18px;
         transition: all 0.2s ease-in-out;
     }
 
     a:hover {
         transition: all 0.2s ease-in-out;
         background-color:#555555;
+    }
+
+    @media (max-width: 600px) {
+        a {
+            padding: 25px 25%;
+            font-size: 18px;
+        }
+    }
+
+    @media (min-width: 600px) {
+        a {
+            padding: 15px 10%;
+        font-size: 18px;
+        }
     }
 `;
