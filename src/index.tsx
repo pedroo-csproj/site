b7d087b2231a1489308b0a503e912030d8de0f66
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-07-10 09:27:20 -0300

    unusead import removed

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
 import App from './App';
