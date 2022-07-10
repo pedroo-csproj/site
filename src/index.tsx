62130b326adb90466792565a66ed12122e68811c
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-07-10 08:47:42 -0300

    about me added to home

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import styled from 'styled-components';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,10 +10,18 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const Layout = styled.body`
+font-family: 'Lato', sans-serif;
+font-family: 'Roboto', sans-serif;
+text-align: center;
+`;
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Layout>
+        <App />
+      </Layout>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/Home/style.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const AboutMe = styled.div`
+    h1 {
+        line-height: 5px;
+    }
+
+    img {
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+    }
+`
+
+export const Description = styled.div`
+    margin-top: 40px;
+
+    p {
+        margin: 0 auto;
+        text-align: justify;
+        max-width: 60%;
+        font-size: 16px;
+    }
+`;
