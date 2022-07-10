0bd3e5b8afad1cd4ae98cce13e745509b957245e
pedroo-csproj <pedrooctavio.dev@outlook.com>
2022-07-10 18:05:18 -0300

    not found page updated

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 import EmailImage from "./email.png";
 import GithubImage from "./github.png";
 import LinkedInImage from "./linkedin.png";
+import NotFoundImage from "./not-found.jpg";
 import TwitterImage from "./twitter.png";
 
-export { EmailImage, GithubImage, LinkedInImage, TwitterImage };
+export { EmailImage, GithubImage, LinkedInImage, NotFoundImage, TwitterImage };
--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import { NotFoundImage } from "../../assets";
+import { Container } from "./style";
+
 export const NotFound: React.FC = () => {
     return (
-        <div>
-            <p>Not Found</p>
-        </div>
+        <Container>
+            <img src={NotFoundImage} title="not found" alt="not found" />
+            <p>wow, looks like that you typed a invalid route fella. <a href="/">Click here</a> to get back.</p>
+        </Container>
     );
 };
